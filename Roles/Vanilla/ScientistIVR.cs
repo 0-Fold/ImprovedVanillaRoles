4f9aafa11c0be2a5dc24c7083e344bcfd5c4e10e
0_Fold <skibidiminecrafters@gmail.com>
2024-07-24 09:30:40 -0500

    Create ScientistIVR.cs

---
+++ Roles/Vanilla/ScientistIVR.cs
@@ -0,0 +1,45 @@
+
+using AmongUs.GameOptions;
+
+namespace IVR.Roles.Vanilla;
+
+internal class ScientistIVR : RoleBase
+{
+    //===========================SETUP================================\\
+    private const int Id = 6200;
+    private static readonly HashSet<byte> playerIdList = [];
+    public static bool HasEnabled => playerIdList.Any();
+    
+    public override CustomRoles ThisRoleBase => CustomRoles.Scientist;
+    public override Custom_RoleType ThisRoleType => Custom_RoleType.CrewmateVanilla;
+    //==================================================================\\
+
+    private static OptionItem BatteryCooldown;
+    private static OptionItem BatteryDuration;
+
+    public override void SetupCustomOption()
+    {
+        Options.SetupRoleOptions(Id, TabGroup.CrewmateRoles, CustomRoles.ScientistTOHE);
+        BatteryCooldown = IntegerOptionItem.Create(Id + 2, GeneralOption.ScientistBase_BatteryCooldown, new(1, 250, 1), 15, TabGroup.CrewmateRoles, false)
+            .SetParent(Options.CustomRoleSpawnChances[CustomRoles.ScientistTOHE])
+            .SetValueFormat(OptionFormat.Seconds);
+        BatteryDuration = IntegerOptionItem.Create(Id + 3, GeneralOption.ScientistBase_BatteryDuration, new(1, 250, 1), 5, TabGroup.CrewmateRoles, false)
+            .SetParent(Options.CustomRoleSpawnChances[CustomRoles.ScientistTOHE])
+            .SetValueFormat(OptionFormat.Seconds);
+    }
+
+    public override void Init()
+    {
+        playerIdList.Clear();
+    }
+    public override void Add(byte playerId)
+    {
+        playerIdList.Add(playerId);
+    }
+
+    public override void ApplyGameOptions(IGameOptions opt, byte playerId)
+    {
+        AURoleOptions.ScientistCooldown = BatteryCooldown.GetInt();
+        AURoleOptions.ScientistBatteryCharge = BatteryDuration.GetInt();
+    }
+}
