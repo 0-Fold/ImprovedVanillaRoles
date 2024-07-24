8a7ea7d5f5bc72841ce2272294a0b6e8016f1b2f
0_Fold <skibidiminecrafters@gmail.com>
2024-07-24 12:52:30 -0500

    Create TextOptionItem.cs

---
+++ Modules/OptionItem/TextOptionItem.cs
@@ -0,0 +1,17 @@
+namespace IVR;
+
+public class TextOptionItem : OptionItem
+{
+    public IntegerValueRule Rule;
+
+    public TextOptionItem(int id, string name, TabGroup tab, int defaultValue = 0, bool isSingleValue = false) : base(id, name, defaultValue, tab, isSingleValue)
+    {
+        IsText = true;
+        IsHeader = true;
+    }
+
+    // Getter
+    public override int GetInt() => Rule.GetValueByIndex(CurrentValue);
+    public override float GetFloat() => Rule.GetValueByIndex(CurrentValue);
+    public override string GetString() => Translator.GetString(Name);
+}
