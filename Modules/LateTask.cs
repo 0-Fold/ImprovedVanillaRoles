6e9f003912d3bd0280533afde2d53b5d5cc57266
0_Fold <skibidiminecrafters@gmail.com>
2024-07-24 13:00:08 -0500

    Create LateTask.cs

---
+++ Modules/LateTask.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+
+namespace IVR;
+
+class LateTask
+{
+    private static readonly List<LateTask> Tasks = [];
+    private readonly Action action;
+    private readonly bool log;
+    private readonly string name;
+    private float timer;
+
+    /// <summary>
+    /// Creates a task that will be automatically completed after the specified amount of time
+    /// </summary>
+    /// <param name="action">The delayed task</param>
+    /// <param name="time">The time to wait until the task is run</param>
+    /// <param name="name">The name of the task</param>
+    /// <param name="log">Whether to send log of the creation and completion of the Late Task</param>
+    private LateTask(Action action, float time, string name, bool log)
+    {
+        this.action = action;
+        timer = time;
+        this.name = name;
+        this.log = log;
+        Tasks.Add(this);
+        if (log && name is not "" and not "No Name Task")
+            Logger.Info("\"" + name + "\" is created", "LateTask");
+    }
+
+    private bool Run(float deltaTime)
+    {
+        timer -= deltaTime;
+        if (timer <= 0)
+        {
+            action();
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void New(Action action, float time, string name = "No Name Task", bool log = true) => _ = new LateTask(action, time, name, log);
+
+    public static void Update(float deltaTime)
+    {
+        foreach (var task in Tasks.ToArray())
+        {
+            try
+            {
+                if (task.Run(deltaTime))
+                {
+                    if (task.name != string.Empty && task.log)
+                        Logger.Info($"\"{task.name}\" is finished", "LateTask");
+                    Tasks.Remove(task);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error($"{ex.GetType()}: {ex.Message}  in \"{task.name}\"\n{ex.StackTrace}", "LateTask.Error", false);
+                Tasks.Remove(task);
+            }
+        }
+    }
+}
