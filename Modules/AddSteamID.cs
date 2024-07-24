0154ca79019aebf161abe93fcf955d1aea8d1a74
0_Fold <skibidiminecrafters@gmail.com>
2024-07-24 12:53:58 -0500

    Create AddSteamID.cs

---
+++ Modules/AddSteamID.cs
@@ -0,0 +1,29 @@
+using System;
+using System.IO;
+using static IVR.Translator;
+
+namespace IVR;
+
+public static class AddSteamID
+{
+    private const string FilePath = "./steam_appid.txt";
+
+    public static void AddSteamAppIdFile()
+    {
+        try
+        {
+            if (!File.Exists(FilePath))
+            {
+                Logger.Warn("Creating a new steam_appid.txt file", "AddSteamID");
+                File.Create(FilePath).Close();
+                File.WriteAllText(FilePath, "945360");
+
+                ModUpdater.ShowPopup(GetString("AppIDAdded"), StringNames.Close, true);
+            }
+        }
+        catch (Exception e)
+        {
+            Utils.ThrowException(e);
+        }
+    }
+}
