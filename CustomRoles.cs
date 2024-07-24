fe94d79cccab25d30c5557418eb407ea15b59a30
0_Fold <skibidiminecrafters@gmail.com>
2024-07-24 13:12:28 -0500

    Create CustomRoles.cs

---
+++ CustomRoles.cs
@@ -0,0 +1,31 @@
+namespace IVR;
+
+public enum CustomRoles
+{
+    // Default
+    Crewmate = 0,
+
+    // Impostors (Vanilla)
+    Impostor,
+    Phantom,
+    Shapeshifter,
+
+    // Vanilla Remakes
+    ImpostorIVR,
+    PhantomIVR,
+    ShapeshifterIVR,
+
+// Crewmates (Vanilla)
+    Engineer,
+    GuardianAngel,
+    Noisemaker,
+    Scientist,
+    Tracker,
+
+    // Vanilla Remakes
+    CrewmateIVR,
+    EngineerIVR,
+    GuardianAngelIVR,
+    NoisemakerIVR,
+    ScientistEIVR,
+    TrackerIVR,
