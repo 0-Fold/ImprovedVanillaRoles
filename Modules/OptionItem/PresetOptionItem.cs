c6ab30b4449e04e40aa09e2143a50636cfa422cd
0_Fold <skibidiminecrafters@gmail.com>
2024-07-24 09:52:36 -0500

    Create PresetOptionItem.cs

---
+++ Modules/OptionItem/PresetOptionItem.cs
@@ -0,0 +1,32 @@
+namespace IVR;
+
+public class PresetOptionItem(int defaultValue, TabGroup tab) : OptionItem(0, "Preset", defaultValue, tab, true)
+{
+    public readonly IntegerValueRule Rule = (0, NumPresets - 1, 1);
+
+    // Getter
+    public override int GetInt() => Rule.GetValueByIndex(CurrentValue);
+    public override float GetFloat() => Rule.GetValueByIndex(CurrentValue);
+
+    public override string GetString()
+    {
+        return CurrentValue switch
+        {
+            0 => Main.Preset1.Value == (string)Main.Preset1.DefaultValue ? Translator.GetString("Preset_1") : Main.Preset1.Value,
+            1 => Main.Preset2.Value == (string)Main.Preset2.DefaultValue ? Translator.GetString("Preset_2") : Main.Preset2.Value,
+            2 => Main.Preset3.Value == (string)Main.Preset3.DefaultValue ? Translator.GetString("Preset_3") : Main.Preset3.Value,
+            3 => Main.Preset4.Value == (string)Main.Preset4.DefaultValue ? Translator.GetString("Preset_4") : Main.Preset4.Value,
+            4 => Main.Preset5.Value == (string)Main.Preset5.DefaultValue ? Translator.GetString("Preset_5") : Main.Preset5.Value,
+            _ => null
+        };
+    }
+
+    public override int GetValue() => Rule.RepeatIndex(base.GetValue());
+
+    // Setter
+    public override void SetValue(int value, bool doSync = true)
+    {
+        base.SetValue(Rule.RepeatIndex(value), doSync);
+        SwitchPreset(Rule.RepeatIndex(value));
+    }
+}
