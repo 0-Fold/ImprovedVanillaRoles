cd62d42cdf4f9d3c78d6dd5907b3fd140d5d3206
0_Fold <skibidiminecrafters@gmail.com>
2024-07-24 02:39:06 -0500

    Create VanillaRole.cs

---
+++ Roles/VanillaRole.cs
@@ -0,0 +1,18 @@
+namespace IVR
+{
+    internal class VanillaRole : RoleBase
+    {
+        public static bool On;
+        public override bool IsEnable => On;
+
+        public override void Init()
+        {
+            On = false;
+        }
+
+        public override void Add(byte playerId)
+        {
+            On = true;
+        }
+    }
+}
