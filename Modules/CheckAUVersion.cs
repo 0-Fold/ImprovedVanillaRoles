cbe95a6f0d057743d12bc128dc82fd4d8846a62d
0_Fold <skibidiminecrafters@gmail.com>
2024-07-24 12:59:07 -0500

    Create CheckAUVersion.cs

---
+++ Modules/CheckAUVersion.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+namespace IVR.Modules;
+
+// https://github.com/tukasa0001/TownOfHost/blob/main/Modules/VersionChecker.cs
+public static class VersionChecker
+{
+    public static bool IsSupported { get; private set; } = true;
+
+    public static void Check()
+    {
+        var AmongUsVersion = Version.Parse(Application.version);
+        Logger.Info($" {AmongUsVersion}", "Among Us Version Check");
+
+        var SupportedVersion = Version.Parse(Main.SupportedAUVersion);
+        Logger.Info($" {SupportedVersion}", "Supported Version Check");
+
+        IsSupported = AmongUsVersion >= SupportedVersion;
+        Logger.Info($" {IsSupported}", "Version Is Supported?");
+
+        if (!IsSupported)
+        {
+            ErrorText.Instance.AddError(ErrorCode.UnsupportedVersion);
+        }
+    }
+}
