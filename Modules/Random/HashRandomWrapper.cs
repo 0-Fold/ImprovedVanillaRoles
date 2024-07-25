38b2a2b07b0fb9ad8268ad6c6a83c7210d906dea
0_Fold <skibidiminecrafters@gmail.com>
2024-07-24 23:09:17 -0500

    Create HashRandomWrapper.cs

---
+++ Modules/Random/HashRandomWrapper.cs
@@ -0,0 +1,10 @@
+#pragma warning disable CA1822
+namespace IVR;
+
+public class HashRandomWrapper : IRandom
+{
+    public int Next(int minValue, int maxValue) => HashRandom.Next(minValue, maxValue);
+    public int Next(int maxValue) => HashRandom.Next(maxValue);
+    public uint Next() => HashRandom.Next();
+    public int FastNext(int maxValue) => HashRandom.FastNext(maxValue);
+}
