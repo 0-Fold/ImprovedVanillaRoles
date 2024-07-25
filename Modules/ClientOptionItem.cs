e60e57217f9b9cdd7cf0afd121c935f593990fa9
0_Fold <skibidiminecrafters@gmail.com>
2024-07-24 23:19:26 -0500

    Update ClientOptionItem.cs

--- Modules/ClientOptionItem.cs
+++ Modules/ClientOptionItem.cs
@@ -3,7 +3,7 @@ using System;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
-namespace TOHE;
+namespace IVR;
 
 //来源：https://github.com/tukasa0001/TownOfHost/pull/1265
 public class ClientOptionItem
@@ -64,8 +64,8 @@ public class ClientOptionItem
 
                 var modOptionsButton = Object.Instantiate(mouseMoveToggle, generalTab);
                 modOptionsButton.transform.localPosition = leaveButton?.transform?.localPosition ?? new(0f, -2.4f, 1f);
-                modOptionsButton.name = "TOHEOptions";
-                modOptionsButton.Text.text = Translator.GetString("TOHEOptions");
+                modOptionsButton.name = "IVROptions";
+                modOptionsButton.Text.text = Translator.GetString("IVROptions");
                 modOptionsButton.Background.color = new Color32(255, 192, 203, byte.MaxValue);
                 var modOptionsPassiveButton = modOptionsButton.GetComponent<PassiveButton>();
                 modOptionsPassiveButton.OnClick = new();
