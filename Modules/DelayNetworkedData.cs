bf2259cabbb6cca199753acaa5c62a3f069ce80a
0_Fold <skibidiminecrafters@gmail.com>
2024-07-24 23:20:46 -0500

    Update DelayNetworkedData.cs

--- Modules/DelayNetworkedData.cs
+++ Modules/DelayNetworkedData.cs
@@ -3,7 +3,7 @@ using InnerNet;
 using System;
 using UnityEngine;
 
-namespace TOHE.Modules.DelayNetworkDataSpawn;
+namespace IVR.Modules.DelayNetworkDataSpawn;
 
 [HarmonyPatch(typeof(InnerNetClient))]
 public class InnerNetClientPatch
