4f1ad0ea04180beaec908527583e6a18051d5c3e
0_Fold <skibidiminecrafters@gmail.com>
2024-07-24 23:08:00 -0500

    Update BooleanOptionItem.cs

--- Modules/OptionItem/BooleanOptionItem.cs
+++ Modules/OptionItem/BooleanOptionItem.cs
@@ -1,4 +1,4 @@
-namespace EHR;
+namespace IVR;
 
 public class BooleanOptionItem(int id, string name, bool defaultValue, TabGroup tab, bool isSingleValue = false) : OptionItem(id, name, defaultValue ? 1 : 0, tab, isSingleValue)
 {
